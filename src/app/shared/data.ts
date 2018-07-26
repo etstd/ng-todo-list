2d9a8ef462668f89a53bcd9d1bf2d560771f1467
es <studenteternal@protonmail.com>
2018-07-26 08:51:44 +0300

    Remember =)

--- src/app/shared/data.ts
+++ src/app/shared/data.ts
@@ -15,5 +15,53 @@ export const todos = [
     id: 2,
     completed: false,
     title: "Прибраться в стилях"
-  }
+  },
+  
+  {
+    id: 3,
+    completed: false,
+    title: "Прочитать доки про HTTP запросы Angular"
+  },
+  
+  {
+    id: 4,
+    completed: false,
+    title: "Прочитать доки про RxJS"
+  },
+
+  {
+    id: 5,
+    completed: false,
+    title: "Прибраться в стилях"
+  },
+  {
+    id: 6,
+    completed: false,
+    title: "Прочитать доки про HTTP запросы Angular"
+  },
+  
+  {
+    id: 7,
+    completed: false,
+    title: "Прочитать доки про RxJS"
+  },
+
+  {
+    id: 8,
+    completed: false,
+    title: "Прибраться в стилях"
+  },
+
+  {
+    id: 9,
+    completed: false,
+    title: "Прибраться в стилях"
+  },
+
+  {
+    id: 10,
+    completed: false,
+    title: "Прибраться в стилях"
+  },
+
 ];
